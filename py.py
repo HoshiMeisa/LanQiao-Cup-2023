a1983642d814693ec1eea17020930b347a10d9e6
KanaMeisa <stewartjobs@outlook.com>
2023-06-03 18:40:41 +0800

    add new code

--- py.py
+++ py.py
@@ -1,25 +1,18 @@
 import requests
 import openai
 
-# 替换为您的API密钥
-api_key = "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"
-
-# 设置API密钥
+api_key = "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"
 openai.api_key = api_key
 
-# 发送API请求
 response = openai.Image.create(
-    prompt="一只白色暹罗猫",
+    prompt="anime primary school girl uniform classroom twilight alone sitting window",
     n=1,
     size="1024x1024"
 )
 
-# 获取图片URL
 image_url = response['data'][0]['url']
-
-# 下载并保存图片
 image_response = requests.get(image_url)
 with open("generated_image.jpg", "wb") as img_file:
     img_file.write(image_response.content)
 
-print("图片生成成功！")
+print("Done！")
